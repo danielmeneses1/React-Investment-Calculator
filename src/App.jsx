0cd68e3fdb378483221d2a75276be21d5dc5812f
Daniel Dalessandro Sousa Meneses <dg189948@gmail.com>
2024-09-22 17:12:17 -0300

    feat(app): add functionality to access userInput in App component
    Added state management for userInput in App component.
    Passed userInput state as props to UserInput component.
    Implemented handleChange function to update userInput state.
    Ensured userInput values are accessible and displayed within App component.
    Integrated Results component to receive userInput as props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import Header from "./components/Header"
+import Results from "./components/Results";
 import UserInput from "./components/UserInput"
+import { useState } from "react"
+
 
 function App() {
+
+  const [userInput, setUserInput] =useState({
+    initialInvestment: 0,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+});
+
+const handleChange= (inputIdentifier, newValue) =>{
+  setUserInput(prevUserInput => {
+      return {
+          ...prevUserInput,
+          [inputIdentifier]: newValue
+      }
+  })
+  
+}
+
+//Cada vez que o usuário digitar algo no campo, o estado do React será atualizado com o novo valor para initialInvestment, 
+//e esse valor atualizado será refletido no input, criando uma ligação contínua entre o estado e a interface.
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput = {userInput} onChange={handleChange}/>
+      <Results input={userInput}/>
     </>
   )
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,15 +1,9 @@
 //this component is used to take user input, basically
 
-import { useState, useEffect} from "react"
+import { useEffect} from "react"
 
-export default function UserInput(){
-    const [userInput, setUserInput] =useState({
-        initialInvestment: 0,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    });
-
+export default function UserInput({ onChange, userInput}) {
+    
     useEffect(() => {
         console.log("Initial Investment: ", userInput.initialInvestment);
         console.log("Annual Investment: ", userInput.annualInvestment);
@@ -17,18 +11,7 @@ export default function UserInput(){
         console.log("Duration: ", userInput.duration);
     }, [userInput]); // Executa sempre que userInput mudar
 
-    const handleChange= (inputIdentifier, newValue) =>{
-        setUserInput(prevUserInput => {
-            return {
-                ...prevUserInput,
-                [inputIdentifier]: newValue
-            }
-        })
-        
-    }
-
-    //Cada vez que o usuário digitar algo no campo, o estado do React será atualizado com o novo valor para initialInvestment, 
-    //e esse valor atualizado será refletido no input, criando uma ligação contínua entre o estado e a interface.
+    
     return <section id="user-input">
         <div className="input-group">
             <p>
@@ -38,7 +21,7 @@ export default function UserInput(){
                 required 
                 value={userInput.initialInvestment}
                 onChange={(event)=>
-                    handleChange('initialInvestment',event.target.value)
+                    onChange('initialInvestment',event.target.value)
                 }
                     
                 />
@@ -48,7 +31,7 @@ export default function UserInput(){
                 <input type="number" required
                 value={userInput.annualInvestment}
                 onChange={(event)=>
-                    handleChange('annualInvestment',event.target.value)
+                    onChange('annualInvestment',event.target.value)
                 }
                 />
             </p>
@@ -57,7 +40,7 @@ export default function UserInput(){
                 <input type="number" required
                 value={userInput.expectedReturn}
                 onChange={(event)=>
-                    handleChange('expectedReturn',event.target.value)
+                    onChange('expectedReturn',event.target.value)
                 }
                 />
             </p>
@@ -66,7 +49,7 @@ export default function UserInput(){
                 <input type="number" required
                 value={userInput.duration}
                 onChange={(event)=>
-                    handleChange('duration',event.target.value)
+                    onChange('duration',event.target.value)
                 }
                 />
             </p>
